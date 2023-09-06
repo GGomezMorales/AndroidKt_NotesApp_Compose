53ce9febbf71e1c8f537aeda5508239be82e15b2
GGomezMorales <gugomog3@gmail.com>
2023-09-05 23:24:10 -0500

    feat: added first version snackbar

--- app/src/main/java/com/ggomezmorales/notesapp/MainActivity.kt
+++ app/src/main/java/com/ggomezmorales/notesapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ggomezmorales.notesapp
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,6 +9,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,11 +17,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,6 +39,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
@@ -155,5 +167,60 @@ fun InputTextArea(
             value = textValue,
             onValueChange = onTextChange
         )
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "RememberReturnType")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFieldButtonsAndSnackbarsCompose(
+) {
+
+    val snackHostState = remember {
+        SnackbarHostState()
+    }
+
+    val scope = rememberCoroutineScope()
+
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(snackHostState) }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+        ) {
+            TextField(
+                value = textFieldState,
+                onValueChange = {
+                    textFieldState = it
+                },
+                label = {
+                    Text(text = "Enter your name")
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    scope.launch {
+                        snackHostState.showSnackbar(
+                            "Hello, ${textFieldState}",
+                            duration = SnackbarDuration.Long
+                        )
+                    }
+                }
+            ) {
+                Text(text = "Please greet me")
+            }
+        }
     }
 }
